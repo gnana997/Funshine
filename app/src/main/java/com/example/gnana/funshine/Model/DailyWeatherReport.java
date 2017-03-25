f44c93f31a4dcd47fef85b3255aedfdb123d3dbd
Gnana Varadaraju <Gnana Varadaraju>
2017-03-25 19:26:55 +0530

    First commit

---
+++ app/src/main/java/com/example/gnana/funshine/Model/DailyWeatherReport.java
@@ -0,0 +1,67 @@
+package com.example.gnana.funshine.Model;
+
+import com.android.volley.Request;
+
+/**
+ * Created by gnana on 25-03-2017.
+ */
+
+public class DailyWeatherReport {
+
+    public static final String WEATHER_TYPE_CLOUDS = "Clouds";
+    public static final String WEATHER_TYPE_CLEAR = "Clear";
+    public static final String WEATHER_TYPE_RAIN = "Rain";
+    public static final String WEATHER_TYPE_WIND = "Wind";
+    public static final String WEATHER_TYPE_SNOW = "Snow";
+
+    private String cityName;
+    private String country;
+    private int currentTemp;
+    private int maxTemp;
+    private int minTemp;
+    private String weather;
+    private String formattedDate;
+
+    public DailyWeatherReport(String cityName, String country, int currentTemp, int maxTemp, int minTemp, String weather, String rawDate) {
+        this.cityName = cityName;
+        this.country = country;
+        this.currentTemp = currentTemp;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.weather = weather;
+        this.formattedDate = rawDatetoFormatted(rawDate);
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
+
+    public int getMaxTemp() {
+        return maxTemp;
+    }
+
+    public int getMinTemp() {
+        return minTemp;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public String getFormattedDate() {
+        return formattedDate;
+    }
+
+    public String rawDatetoFormatted(String rawDate){
+        return "May 1";
+    }
+
+}
--- app/src/main/java/com/example/gnana/funshine/WeatherActivity.java
+++ app/src/main/java/com/example/gnana/funshine/WeatherActivity.java
@@ -9,9 +9,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,13 +22,18 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.gnana.funshine.Model.DailyWeatherReport;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks , GoogleApiClient.OnConnectionFailedListener , LocationListener {
 
@@ -34,8 +42,17 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     final String URL_UNITS = "&units=metric";
     final String URL_API_KEY = "&APPID=f8f318b8ad41dabc878dec49cc676c91";
     final int Permission_Location = 111;
-    private GoogleApiClient mGoogleApiClient;
 
+    private ImageView weatherIcon;
+    private ImageView weatherminiIcon;
+    private TextView weatherDate;
+    private TextView currentTemp;
+    private TextView lowTemp;
+    private TextView country;
+    private TextView weatherDesc;
+
+    private GoogleApiClient mGoogleApiClient;
+    private ArrayList<DailyWeatherReport> weatherReport = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,15 +65,48 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
                 .addOnConnectionFailedListener(this)
                 .build();
 
+        weatherIcon = (ImageView)findViewById(R.id.weatherIcon);
+        weatherminiIcon = (ImageView)findViewById(R.id.weatherminiIcon);
+        weatherDate = (TextView)findViewById(R.id.weatherDate);
+        currentTemp = (TextView)findViewById(R.id.currentTemp);
+        lowTemp = (TextView)findViewById(R.id.lowTemp);
+        country = (TextView)findViewById(R.id.country);
+        weatherDesc = (TextView)findViewById(R.id.weatherDesc);
+
     }
 
     public void  downloadData(Location location){
         final String UrlCoords = URL_COORDS + location.getLatitude() + "&lon=" + location.getLongitude();
         final String url = URL_BASE + UrlCoords + URL_UNITS + URL_API_KEY;
+        Log.v("Url","url:" + url);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                Log.v("Funshine","Res:" + response.toString());
+               try{
+                   JSONObject city = response.getJSONObject("city");
+                   String cityname = city.getString("name");
+                   String country = city.getString("country");
+                   JSONArray list = response.getJSONArray("list");
+                   for (int x = 0; x <= 4 ; x++ ){
+                       JSONObject obj = list.getJSONObject(x);
+                       JSONObject main = obj.getJSONObject("main");
+                       Double temp = main.getDouble("temp");
+                       Double temp_min = main.getDouble("temp_min");
+                       Double temp_max = main.getDouble("temp_max");
+                       JSONArray weatherdata = obj.getJSONArray("weather");
+                       JSONObject weather = weatherdata.getJSONObject(0);
+                       String desc = weather.getString("main");
+                       String rawDate = obj.getString("dt_txt");
+
+                       DailyWeatherReport report = new DailyWeatherReport(cityname,country,temp.intValue(),temp_max.intValue(),temp_min.intValue(),desc,rawDate);
+                       weatherReport.add(report);
+                   }
+
+               } catch (JSONException e){
+                    Log.v("JSON","Exc: " + e.getLocalizedMessage());
+               }
+                    updateUI();
+
             }
         }, new Response.ErrorListener() {
             @Override
@@ -66,6 +116,37 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
         });
 
         Volley.newRequestQueue(this).add(jsonObjectRequest);
+    }
+
+    public  void  updateUI(){
+        if(weatherReport.size() > 0){
+            DailyWeatherReport report = weatherReport.get(0);
+
+            switch (report.getWeather()){
+                case DailyWeatherReport.WEATHER_TYPE_CLOUDS:
+                    weatherIcon.setImageDrawable(getResources().getDrawable(R.drawable.cloudy));
+                    weatherminiIcon.setImageDrawable(getResources().getDrawable(R.drawable.cloudy));
+                    break;
+                case  DailyWeatherReport.WEATHER_TYPE_RAIN:
+                    weatherIcon.setImageDrawable(getResources().getDrawable(R.drawable.rainy));
+                    weatherminiIcon.setImageDrawable(getResources().getDrawable(R.drawable.rainy));
+                    break;
+                case  DailyWeatherReport.WEATHER_TYPE_SNOW:
+                    weatherIcon.setImageDrawable(getResources().getDrawable(R.drawable.snow));
+                    weatherminiIcon.setImageDrawable(getResources().getDrawable(R.drawable.snow));
+                    break;
+                default:
+                    weatherIcon.setImageDrawable(getResources().getDrawable(R.drawable.sunny));
+                    weatherminiIcon.setImageDrawable(getResources().getDrawable(R.drawable.sunny));
+            }
+
+            weatherDate.setText("Today ,May 1");
+            currentTemp.setText(Integer.toString(report.getMaxTemp()));
+            lowTemp.setText(Integer.toString(report.getMinTemp()));
+            country.setText(report.getCityName()+","+report.getCountry());
+            weatherDesc.setText(report.getWeather());
+
+        }
     }
 
     @Override
